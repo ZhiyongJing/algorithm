fcf4a75f00ad9671e45d145d2aefba52cffcd617
zjing <ZHIYONGJING@OUTLOOK.COM>
2023-12-06 23:13:29 -0500

    add question

---
+++ src/leetcode/frequent/dfs/LeetCode_104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,80 @@
+package leetcode.frequent.dfs;
+
+import leetcode.frequent.TreeNode;
+
+/**
+  *@Question:  104. Maximum Depth of Binary Tree     
+  *@Difculty:  1 [1->Easy, 2->Medium, 3->Hard]
+  *@Frequency: 52.94%      
+  *@Time Complexity: O
+  *@Space Complexity: O
+ */
+
+public class LeetCode_104_MaximumDepthOfBinaryTree{
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return java.lang.Math.max(left_height, right_height) + 1;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new LeetCode_104_MaximumDepthOfBinaryTree().new Solution();
+        // TO TEST
+        //solution.
+    }
+}
+/**
+<p>Given the <code>root</code> of a binary tree, return <em>its maximum depth</em>.</p>
+
+<p>A binary tree's <strong>maximum depth</strong>&nbsp;is the number of nodes along the longest path from the root node down to the farthest leaf node.</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p> 
+<img alt="" src="https://assets.leetcode.com/uploads/2020/11/26/tmp-tree.jpg" style="width: 400px; height: 277px;" /> 
+<pre>
+<strong>Input:</strong> root = [3,9,20,null,null,15,7]
+<strong>Output:</strong> 3
+</pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> root = [1,null,2]
+<strong>Output:</strong> 2
+</pre>
+
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li>The number of nodes in the tree is in the range <code>[0, 10<sup>4</sup>]</code>.</li> 
+ <li><code>-100 &lt;= Node.val &lt;= 100</code></li> 
+</ul>
+
+<div><div>Related Topics</div><div><li>Tree</li><li>Depth-First Search</li><li>Breadth-First Search</li><li>Binary Tree</li></div></div><br><div><li>👍 12163</li><li>👎 203</li></div>
+*/

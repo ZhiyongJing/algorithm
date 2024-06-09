515d4f59e84f3ff66404d14c7d2c2117bb06c4d4
zjing <ZHIYONGJING@OUTLOOK.COM>
2024-06-09 12:15:32 -0400

    add question

--- src/main/java/interview/company/bloomberg/BloombergQuestions.java
+++ src/main/java/interview/company/bloomberg/BloombergQuestions.java
@@ -2,12 +2,21 @@ package interview.company.bloomberg;
 
 // Interview questions
 
-//import leetcode.question.dfs.LeetCode_39_CombinationSum;//优化memorization
-//import leetcode.question.dp.LeetCode_322_CoinChange; //返回最小硬币数量的所有组合
-//import leetcode.question.dp.LeetCode_518_CoinChangeIi;
-//import leetcode.question.linked_list.LeetCode_430_FlattenAMultilevelDoublyLinkedList;//问时间复杂度
+import leetcode.question.dfs.LeetCode_39_CombinationSum;
+import leetcode.question.dp.LeetCode_322_CoinChange;
+import leetcode.question.dp.LeetCode_518_CoinChangeIi;
+import leetcode.question.linked_list.LeetCode_430_FlattenAMultilevelDoublyLinkedList;
 
+import java.util.ArrayList;
+import java.util.List;
+public class BloombergQuestions {
+    public static void main(String[] args) {
+        List<Object> questions = new ArrayList<>();
+        questions.add(new LeetCode_39_CombinationSum());
+        questions.add(new LeetCode_518_CoinChangeIi());
+        questions.add(new LeetCode_322_CoinChange());
+        questions.add(new LeetCode_518_CoinChangeIi());
+        questions.add(new LeetCode_430_FlattenAMultilevelDoublyLinkedList());
 
-
-//public class BloombergQuestions {
-//}
+    }
+}

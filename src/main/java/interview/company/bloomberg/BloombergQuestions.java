973070ceb70aacb718c421e9774cc1e414ef00f0
zjing <ZHIYONGJING@OUTLOOK.COM>
2024-06-10 15:34:31 -0400

    add question

--- src/main/java/interview/company/bloomberg/BloombergQuestions.java
+++ src/main/java/interview/company/bloomberg/BloombergQuestions.java
@@ -14,13 +14,12 @@ import java.util.List;
 public class BloombergQuestions {
     public static void main(String[] args) {
         List<Object> questions = new ArrayList<>();
-        questions.add(new LeetCode_39_CombinationSum());
-        questions.add(new LeetCode_518_CoinChangeIi());
-        questions.add(new LeetCode_322_CoinChange());
-        questions.add(new LeetCode_518_CoinChangeIi());
-        questions.add(new LeetCode_430_FlattenAMultilevelDoublyLinkedList());
-        questions.add(new LeetCode_1396_DesignUndergroundSystem());
-        questions.add(new LeetCode_656_CoinPath());
+        questions.add(new LeetCode_39_CombinationSum());//优化memorization
+        questions.add(new LeetCode_322_CoinChange());//返回最小硬币数量的所有组合，无限硬币
+        questions.add(new LeetCode_518_CoinChangeIi());//返回所有硬币数量组合
+        questions.add(new LeetCode_430_FlattenAMultilevelDoublyLinkedList());//问时间复杂度
+        questions.add(new LeetCode_1396_DesignUndergroundSystem());//做完问了一些拓展的问题 比如如果有人只刷进站不刷出站怎么办 反着怎么办
+        questions.add(new LeetCode_656_CoinPath());//
 
     }
 }

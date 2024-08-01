476032f0ba7ba8039c93c22985281b441f7300ef
zjing <ZHIYONGJING@OUTLOOK.COM>
2024-07-31 22:07:39 -0400

    add question

---
+++ src/main/java/interview/microsoft/MicrosoftQuestions.java
@@ -0,0 +1,16 @@
+package interview.microsoft;
+
+import leetcode.question.dfs.LeetCode_39_CombinationSum;
+import leetcode.question.dfs.LeetCode_40_CombinationSumIi;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MicrosoftQuestions {
+    public static void main(String[] args) {
+        List<Object> questions = Arrays.asList(
+                new LeetCode_39_CombinationSum(),
+                new LeetCode_40_CombinationSumIi(),
+                new LeetCode_216
+
+}
